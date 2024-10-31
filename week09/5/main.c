9a7db374897d57257d39b4d3b7e5ef92a11c87a2
hyshin9 <sshyeon@sookmyung.ac.kr>
2024-10-31 17:55:05 +0900

    0

---
+++ week09/5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int all_files;
+
+extern void sub(); // static -> compile x
+
+int main(void) {
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+}
+
+/*
+#include <stdio.h>
+
+static int sum=1; //static -> compile x
+
+int main(void)
+{
+	int sum=0;
+	
+	printf("sum= %d\n", sum);
+	
+	return 0;
+}
+*/
