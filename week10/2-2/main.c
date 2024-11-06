da55ffe10734c4325543f9ae951584b80869ee0b
hyshin9 <sshyeon@sookmyung.ac.kr>
2024-11-06 14:36:12 +0900

    1 2(1,2) 3(1,2) 4

---
+++ week10/2-2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) 
+{
+	int i, average;
+	int sum;
+	int grade[SIZE];
+	
+	sum=0;
+	for(i=0; i<SIZE; i++)
+	{
+		printf("학생 성적을 입력하세요:");
+		scanf("%d", &grade[i]);
+		sum += grade[i];
+	}
+	average=sum/SIZE;
+	printf("성적평균: %d\n", average);
+	return 0;
+}
