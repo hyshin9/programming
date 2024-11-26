23534f86fc354b007119879532d8efe0abf32c8c
hyshin9 <sshyeon@sookmyung.ac.kr>
2024-11-27 02:11:33 +0900

    w12

--- week12/7/main.c
+++ week12/7/main.c
@@ -3,13 +3,14 @@
 
 char *proverb="All that glisters is not gold.";
 
-void setPointer(char *q){
-	q=proverb;
+void setPointer(char **q){
+	*q=proverb; 
 }
+
 int main(void){
 	char *p="zzz";
-	setPointer(p);
-	printf("%s\n", p); // -> print zzz
+	setPointer(&p); // q -> &p
+	printf("%s\n", p); // first code -> print zzz
 	
 	return 0;
 }
